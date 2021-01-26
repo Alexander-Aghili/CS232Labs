9395f00ea84a4b8afa49ef7397c7c15a204740c8
Alexander.Aghili <alexander.w.aghili@gmail.com>
2021-01-25 19:32:01 -0800

    Commitment

---
+++ src/First.java
@@ -0,0 +1,16 @@
+public class First {
+
+	public static void main(String[] args) {
+		int hours, minutes, seconds;
+		for(hours = 0; hours < 24; hours++) {
+			for (minutes = 0; minutes < 60; minutes++) { 
+				for (seconds = 0; seconds < 60; seconds++)  { 
+					System.out.println(hours);
+					System.out.println(minutes);   
+					System.out.println(seconds); 
+				} 
+			}
+		}
+	}
+
+}
