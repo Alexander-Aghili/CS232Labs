31aac78da8d2b78e21e05d5424c2c0f542035cea
Alexander Aghili <43054019+Alexander-Aghili@users.noreply.github.com>
2021-01-28 14:35:26 -0800

    Update OTP.java

--- src/OTP.java
+++ src/OTP.java
@@ -1,29 +1,32 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Random;
+import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 //Alexander Aghili
 
 public class OTP extends JFrame implements ActionListener {
 
+	private static ArrayList<CompanyAuth> companyAuths = new ArrayList<CompanyAuth>();
+	private static ArrayList<JLabel> labels = new ArrayList<JLabel>();
+	private static ArrayList<JButton> refreshButtons = new ArrayList<JButton>();
 	
-	private JButton new_OTP;
-	private JLabel resultLabel;
+	
+	private JButton addCompanyButton;
+	private JTextField addCompanyText;
 	
 	public OTP() {
-		new_OTP = new JButton("Refresh");
-		resultLabel = new JLabel("Your OTP Code");
-		new_OTP.addActionListener(this); // do action on click
+		addCompany();
 		
-		setLayout(new BorderLayout());
-		resultLabel.setHorizontalAlignment(JLabel.CENTER);
-		this.add(new_OTP, BorderLayout.SOUTH);
-		this.add(resultLabel, BorderLayout.CENTER);
+		setLayout(new GridLayout(0, 2));
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocation(250, 100);
 		this.setVisible(true);
@@ -31,17 +34,39 @@ public class OTP extends JFrame implements ActionListener {
 		
 	}
 	
-	
-	private static char[] generate(int len) {
-		String numbers  = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		Random rndNumbers = new Random();
-		char[] otp = new char[len];
-		//Takes random number from the numbers string and adds it to the character array
-		for (int i = 0; i < len; i++) {
-			otp[i] = numbers.charAt(rndNumbers.nextInt(numbers.length()));
-		}
+	private void createLabel(String companyName) {
+		CompanyAuth newCompany = new CompanyAuth(companyName);
+		companyAuths.add(newCompany);
 		
-		return otp;
+		JLabel label = new JLabel("<html>Company: " + companyName
+						+ "<br/>OTP: " + String.valueOf(newCompany.getOTP()) + "</html>");
+		label.setBorder(BorderFactory.createLineBorder(Color.black));
+		this.add(label);
+		labels.add(label);
+		
+		JButton refreshButton = new JButton("Refresh");
+		this.add(refreshButton);
+		refreshButtons.add(refreshButton);
+		refreshButtons.get(refreshButtons.size() - 1).addActionListener(this);	
+		
+		//Refreshes the page
+		this.invalidate();
+		this.validate();
+		this.repaint();
+	}
+	
+	private void addCompany() {
+		addCompanyText = new JTextField();
+		this.add(addCompanyText);
+		addCompanyButton = new JButton("+");
+		addCompanyButton.addActionListener(this);
+		this.add(addCompanyButton);
+	}
+	
+	private static void refreshOTP(int index) {
+		companyAuths.get(index).newOTP();
+		labels.get(index).setText("<html>Company: " + companyAuths.get(index).getCompanyName() 
+				+ "<br/>OTP: " + String.valueOf(companyAuths.get(index).getOTP()) + "</html>");
 	}
 	
 	public static void main(String[] args) {
@@ -51,8 +76,18 @@ public class OTP extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		int len = 10;
-		resultLabel.setText(String.valueOf(OTP.generate(len)));
+		if (e.getSource() == addCompanyButton && !addCompanyText.getText().equals("")) {
+			createLabel(addCompanyText.getText());
+			addCompanyText.setText("");
+			addCompanyText.grabFocus();
+			return;
+		}
+		
+		for(int i = 0; i < refreshButtons.size(); i++) {
+			if (e.getSource() == refreshButtons.get(i)) {
+				refreshOTP(i);
+			}
+		}
 		
 	}
 
